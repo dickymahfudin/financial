3c0fe4b5d463c6f33411f3d8c4ee471589d75805
dickymahfudin <dickymahfudin13@gmail.com>
2021-05-28 18:32:37 +0700

    install bcryptjs

--- src/routes/login.js
+++ src/routes/login.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { user } = require('../models');
 
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 
 router.get('/', (req, res, next) => {
   res.render('login', { title: 'Login', layout: 'layouts/blank' });
